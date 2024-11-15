1c3fd8f76af54e284af5247506fb399cc304e8b8
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-11-15 16:18:27 -0500

    snake setup

--- main.py
+++ main.py
@@ -7,20 +7,26 @@ screen.bgcolor("Black")
 screen.title("Snake Game")
 
 
+starting_positions = [(0,0),(-20,0),(-40,0)]
+
+for position in starting_positions:
+    new_element = Turtle("square")
+    new_element.color("white")
+    new_element.goto(position)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 screen.exitonclick()
-
-
-
-
-
-
-
-
-
-
-
-
