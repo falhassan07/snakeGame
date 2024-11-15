e175064ddf29fcb6d1523b35d20d9d9b42c46c48
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-11-15 16:14:14 -0500

    screen setup

---
+++ main.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+screen.setup(600,600)
+screen.bgcolor("Black")
+screen.title("Snake Game")
+
+
+
+
+
+
+screen.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
